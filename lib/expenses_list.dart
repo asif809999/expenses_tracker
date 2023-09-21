bd70c2d8f42c9e7490eebc58db8dfb48694f760e
Asif <asif809999@gmail.com>
2023-09-21 15:54:29 +0600

    data class inserted into the design class 5.7

--- lib/expenses_list.dart
+++ lib/expenses_list.dart
@@ -2,7 +2,11 @@ import 'package:expense_tracker/model/expense.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+  });
+
   final List<Expense> expenses;
 
   @override
