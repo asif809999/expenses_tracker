5a5483cbd0b106cd1687ac6e8b1e56dc32997e72
Asif <asif809999@gmail.com>
2023-09-26 20:26:12 +0600

    color scheme added 5.28

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
   runApp(
     MaterialApp(
       theme: ThemeData().copyWith(
-          useMaterial3: true,
-          scaffoldBackgroundColor: Color.fromARGB(255, 220, 189, 252)),
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
       home: const Expenses(),
     ),
   );
